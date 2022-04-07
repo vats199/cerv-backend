c9bf10aabd9bf3c909a1de7b9afc28eafe6aa84f
vats199 <vatsalp.tcs@gmail.com>
2022-04-07 14:51:43 +0530

    make it better

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ const db = {};
 
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost',
+    host: 'remotemysql.com',
+    port: 3306,
     operatorAliases: false,
 
     pool: {
