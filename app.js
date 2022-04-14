5b240879768934546097508d9d837f63ef2659f0
vats199 <vatsalp.tcs@gmail.com>
2022-04-14 17:46:08 +0530

    uploading image

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const fileStorage = multer.diskStorage({
       cb(null, 'images');
   },
   filename: (req, file, cb) => {
-      cb(null, new Date().toISOString() + '-' + 'cerv');
+      cb(null, new Date().toISOString() + '-' + file.originalname);
   }
 });
 
