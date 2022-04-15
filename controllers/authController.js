d3993c78832741ed1ed9a35f43a60f6da5ffeafb
vats199 <vatsalp.tcs@gmail.com>
2022-04-15 12:07:10 +0530

    uploading image

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const User = require('../models/user');
 const Store = require('../models/store');
 const Token = require('../models/token');
 exports.postSignup = async (req, res, next) => {
-  console.log(req.file);
+  console.log(req.body);
   if(req.body === {}){
     return console.log("Your Body is empty!")
   }
