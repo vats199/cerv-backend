c1e5f9c20cdf70bb7e24c2439d27ed6d3dca1206
vats199 <vatsalp.tcs@gmail.com>
2022-05-02 10:25:48 +0530

    discount codes

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -26,7 +26,7 @@ exports.getCaterers = async (req,res,next) => {
 
         if(totalCaterers !== 0){
           for(let i=0; i<caterers.length;i++){
-            const rating = await Feedback.findAll({ where: { catererId: caterers[i] } ,order: [Sequelize.fn('AVG', Sequelize.col('rating'))] });
+            const rating = await Feedback.findOne({ where: { catererId: caterers[i] } ,order: [Sequelize.fn('AVG', Sequelize.col('rating'))] });
             caterers[i].rating = rating;
           }
         }
