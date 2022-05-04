7d3d929a80f871c0dee6d699e8fbf3b79040c132
vats199 <vatsalp.tcs@gmail.com>
2022-05-04 17:41:24 +0530

    api integration

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -37,7 +37,7 @@ exports.getCaterers = async (req,res,next) => {
           for(let i=0; i<caterers.length;i++){
             // const rating = await Feedback.findOne({ where: { catererId: caterers[i].userId } ,attributes: [Sequelize.fn('AVG', Sequelize.col('rating'))], raw: true });
             const rating = await db.sequelize.query(`SELECT AVG(rating) as rating FROM feedbacks WHERE catererId = ${caterers[i].userId}`)
-            const avgPri = await db.sequelize.query(`SELECT AVG(price) as avgPrice FROM items WHERE storeId = ${caterers[i].id}`)
+            const avgPri = await db.sequelize.query(`SELECT AVG(price) as avgPrice FROM items WHERE categoryId IN ( SELECT id FROM categories WHERE userId = ${caterers[i].userId})`)
             const avgPrice = Math.floor(avgPri[0][0].avgPrice);
             const decimal = (rating[0][0].rating)%1;
             let rate;
