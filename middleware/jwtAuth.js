4a08b2aa0087bb599defd10a381bf8008270c852
vats199 <vatsalp.tcs@gmail.com>
2022-05-20 09:57:00 +0530

    push notifications

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const { isRegExp } = require('util/types');
+const User  = require('../models/user');
 
 const cert = fs.readFileSync('dev-3sdvkvha.pem')
 
@@ -13,10 +14,12 @@ module.exports = (req, res, next) => {
 
     token = token.split(' ')[1];
 
-    jwt.verify(token, cert ,(err,user)=>{
+    jwt.verify(token, cert ,async (err,user)=>{
         if(!err){
             // console.log(user);
             req.user_id = user.sub.split('|')[1];
+            const loadedUser = await User.findByPk(user.sub.split('|')[1]);
+            req.user = loadedUser;
             next();
         } else{
             return res.status(403).json({error: "User not Authenticated", status: 0})
