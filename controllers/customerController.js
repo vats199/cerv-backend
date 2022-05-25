5852d4f0db16b6dd1be2e05642f0f039363a60a2
vats199 <vatsalp.tcs@gmail.com>
2022-05-25 11:51:23 +0530

    push notifications

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -485,7 +485,7 @@ exports.search = async (req, res, next) => {
   try {
     if (key == 1) {
       const totalResults = await Category.count({ where: { title: { [Op.like]: '%' + term + '%' } } })
-      const results = await Category.findAll({ include: Item, where: { title: { [Op.like]: '%' + term + '%' } } });
+      const results = await Category.findAll({ include: [Item,User], where: { title: { [Op.like]: '%' + term + '%' } } });
 
       return res.status(200).json({
         message: 'Fetched Categories Successfully!',
@@ -497,7 +497,7 @@ exports.search = async (req, res, next) => {
 
     else if (key == 2) {
       const totalResults = await Item.count({ where: { title: { [Op.like]: '%' + term + '%' } } })
-      const results = await Item.findAll({ include: Category, where: { title: { [Op.like]: '%' + term + '%' } } });
+      const results = await Item.findAll({ include: [Category,User], where: { title: { [Op.like]: '%' + term + '%' } } });
 
       return res.status(200).json({
         message: 'Fetched Items Successfully!',
